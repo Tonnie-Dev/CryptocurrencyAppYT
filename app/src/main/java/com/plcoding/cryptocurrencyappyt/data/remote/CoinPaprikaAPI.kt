1e6ab55400c229374f37d8a0cfcbdf0e1161e1a0
Antony <vontonnie@gmail.com>
2021-10-23 10:08:20 +0300

    Add DTO for Coin Details

--- app/src/main/java/com/plcoding/cryptocurrencyappyt/data/remote/CoinPaprikaAPI.kt
+++ app/src/main/java/com/plcoding/cryptocurrencyappyt/data/remote/CoinPaprikaAPI.kt
@@ -1,9 +1,13 @@
 package com.plcoding.cryptocurrencyappyt.data.remote
 
 import retrofit2.http.GET
+import retrofit2.http.Path
 
 interface CoinPaprikaAPI {
 
     @GET("/v1/coins")
     suspend fun getCoins()
+
+    @GET("/v1/coins/{coidID}")
+    suspend fun getCoinById(@Path("coinId"), coinId:String)
 }
