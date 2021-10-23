fc36cfa13aee9e0860a55442d882d48b9b2352a0
Antony <vontonnie@gmail.com>
2021-10-23 06:52:07 +0300

    Create Data Package

---
+++ app/src/main/java/com/plcoding/cryptocurrencyappyt/presentation/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.plcoding.cryptocurrencyappyt.presentation.theme.ui
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import com.plcoding.cryptocurrencyappyt.presentation.theme.CryptocurrencyAppYTTheme
+
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            CryptocurrencyAppYTTheme {
+                Surface(color = MaterialTheme.colors.background) {
+                    
+                }
+            }
+        }
+    }
+}
