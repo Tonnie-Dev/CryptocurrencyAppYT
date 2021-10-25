3210bdd74c64ff0e6d172f75199d9d0bf42e6f55
Antony <vontonnie@gmail.com>
2021-10-25 07:45:56 +0300

    Add CoinRepository inside the Domain Package

---
+++ app/src/main/java/com/plcoding/cryptocurrencyappyt/domain/repository/CoinRepository.kt
@@ -0,0 +1,10 @@
+package com.plcoding.cryptocurrencyappyt.domain.repository
+
+import com.plcoding.cryptocurrencyappyt.data.remote.dto.CoinDTO
+import com.plcoding.cryptocurrencyappyt.data.remote.dto.CoinDetailDTO
+
+interface CoinRepository {
+
+    suspend fun getCoins():List<CoinDTO>
+    suspend fun getCoinsById():CoinDetailDTO
+}
