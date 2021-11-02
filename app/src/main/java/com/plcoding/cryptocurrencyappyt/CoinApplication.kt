8a8e2211c353cdb731a51c3636ca6367c9fd92a0
Antony <vontonnie@gmail.com>
2021-11-02 19:49:29 +0300

    Add Oncreate method

--- app/src/main/java/com/plcoding/cryptocurrencyappyt/CoinApplication.kt
+++ app/src/main/java/com/plcoding/cryptocurrencyappyt/CoinApplication.kt
@@ -2,6 +2,7 @@ package com.plcoding.cryptocurrencyappyt
 
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
+import timber.log.Timber
 
 /*The application class gives Hilt the info about out application
 * so that Hilt also has access to the application context if we
@@ -9,4 +10,10 @@ import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
 class CoinApplication :Application(){
+
+    override fun onCreate() {
+        super.onCreate()
+
+        Timber.plant(Timber.DebugTree())
+    }
 }
