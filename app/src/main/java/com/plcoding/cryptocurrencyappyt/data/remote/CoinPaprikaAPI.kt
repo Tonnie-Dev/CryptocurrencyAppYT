5172ebc5ec8d310960add67179b328efa84473b2
Tonnie <vontonnie@gmail.com>
2022-05-08 17:42:44 +0300

    Update Libs

--- app/src/main/java/com/plcoding/cryptocurrencyappyt/data/remote/CoinPaprikaAPI.kt
+++ app/src/main/java/com/plcoding/cryptocurrencyappyt/data/remote/CoinPaprikaAPI.kt
@@ -2,7 +2,7 @@ package com.plcoding.cryptocurrencyappyt.data.remote
 
 import com.plcoding.cryptocurrencyappyt.data.remote.dto.CoinDTO
 import com.plcoding.cryptocurrencyappyt.data.remote.dto.CoinDetailDTO
-import com.plcoding.cryptocurrencyappyt.domain.model.Coin
+import retrofit2.Call
 import retrofit2.http.GET
 import retrofit2.http.Path
 
@@ -11,10 +11,18 @@ interface CoinPaprikaAPI {
     @GET("/v1/coins")
     suspend fun getCoins(): List<CoinDTO>
 
-   @GET("/v1/coins/{coinId}")
-   suspend fun getCoinById(@Path("coinId") coinId:String):CoinDetailDTO
+    @GET("/v1/coins/{coinId}")
+    suspend fun getCoinById(@Path("coinId") coinId: String): CoinDetailDTO
+
+
+    @GET("group/{id}/users")
+    fun groupList(@Path("id") groupId: Int): Call<List<User>>
+
+
+    data class User(val name: String)
+
 //https://api.coinpaprika.com/v1/coins/
-   // Coin(id=btc-bitcoin
-   //Coin(id=solnet-solana-network
+    // Coin(id=btc-bitcoin
+    //Coin(id=solnet-solana-network
     //Coin(id=ada-cardano
 }
